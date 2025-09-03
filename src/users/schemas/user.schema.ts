ea520b3d9ce2b18822a514ff01737e4eac7901dc
Tran Van Luong <tranvanluong032020@gmail.com>
2025-09-03 21:30:55 +0700

    module: add module user base code

---
+++ src/users/schemas/user.schema.ts
@@ -0,0 +1,30 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+export type UserDocument = HydratedDocument<User>;
+
+@Schema()
+export class User {
+  @Prop({ required: true })
+  name: string;
+
+  @Prop({ required: true })
+  password: string;
+
+  @Prop()
+  phone: string;
+
+  @Prop()
+  age: number;
+
+  @Prop()
+  address: string;
+
+  @Prop()
+  createdAt: Date;
+
+  @Prop()
+  updatedAt: Date;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
