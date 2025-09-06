db304453f4ba3ac6186677effa389cf13a91111c
Tran Van Luong <tranvanluong032020@gmail.com>
2025-09-06 20:20:37 +0700

    guard

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,17 +1,24 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Request } from '@nestjs/common';
 import { AppService } from './app.service';
 import { ConfigService } from '@nestjs/config';
+import { AuthService } from 'src/auth/auth.service';
 
 @Controller()
 export class AppController {
   constructor(
     private readonly appService: AppService,
     private configService: ConfigService,
+    private authService: AuthService,
   ) {}
 
   @Get()
   getHello(): string {
     console.log('check port', this.configService.get<string>('PORT'));
     return this.appService.getHello();
+  }
+
+  @Post('login')
+  async login(@Request() req) {
+    return this.authService.login(req.user);
   }
 }
