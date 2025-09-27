674b5eca02a98606fdc5ca70cb0d79f757951121
Tran Van Luong <tranvanluong032020@gmail.com>
2025-09-27 23:27:05 +0700

    update: resume moduel

--- src/resumes/dto/create-resume.dto.ts
+++ src/resumes/dto/create-resume.dto.ts
@@ -1,1 +1,35 @@
-export class CreateResumeDto {}
+import { IsMongoId, IsNotEmpty } from 'class-validator';
+import mongoose from 'mongoose';
+
+export class CreateResumeDto {
+  @IsNotEmpty({ message: 'Email must not be empty' })
+  email: string;
+
+  @IsNotEmpty({ message: 'UserId must not be empty' })
+  userId: mongoose.Schema.Types.ObjectId;
+
+  @IsNotEmpty({ message: 'URL must not be empty' })
+  url: string;
+
+  @IsNotEmpty({ message: 'Status must not be empty' })
+  status: string;
+
+  @IsNotEmpty({ message: 'CompanyId must not be empty' })
+  companyId: mongoose.Schema.Types.ObjectId;
+
+  @IsNotEmpty({ message: 'JobId must not be empty' })
+  jobId: mongoose.Schema.Types.ObjectId;
+}
+
+export class CreateUserCvDto {
+  @IsNotEmpty({ message: 'URL must not be empty' })
+  url: string;
+
+  @IsNotEmpty({ message: 'CompanyId must not be empty' })
+  @IsMongoId({ message: 'CompanyId must be a valid Mongo ObjectId' })
+  companyId: mongoose.Schema.Types.ObjectId;
+
+  @IsNotEmpty({ message: 'JobId must not be empty' })
+  @IsMongoId({ message: 'JobId must be a valid Mongo ObjectId' })
+  jobId: mongoose.Schema.Types.ObjectId;
+}
--- src/resumes/resumes.controller.ts
+++ src/resumes/resumes.controller.ts
@@ -1,34 +1,64 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  Query,
+} from '@nestjs/common';
 import { ResumesService } from './resumes.service';
-import { CreateResumeDto } from './dto/create-resume.dto';
+import { CreateResumeDto, CreateUserCvDto } from './dto/create-resume.dto';
 import { UpdateResumeDto } from './dto/update-resume.dto';
+import { Public, ResponseMessage, User } from 'src/decorators/customize';
+import { IUser } from 'src/users/user.interface';
 
 @Controller('resumes')
 export class ResumesController {
   constructor(private readonly resumesService: ResumesService) {}
 
   @Post()
-  create(@Body() createResumeDto: CreateResumeDto) {
-    return this.resumesService.create(createResumeDto);
+  @ResponseMessage('Create a new resume')
+  create(@Body() create: CreateUserCvDto, @User() user: IUser) {
+    return this.resumesService.create(create, user);
   }
 
+  @Public()
   @Get()
-  findAll() {
-    return this.resumesService.findAll();
+  @ResponseMessage('Fetch all resumes with paginate')
+  findAll(
+    @Query('current') currentPage: string,
+    @Query('pageSize') pageSize: string,
+    @Query() qs: string,
+  ) {
+    return this.resumesService.findAll(+currentPage, +pageSize, qs);
   }
 
   @Get(':id')
+  @ResponseMessage('Fetch a resume by id')
   findOne(@Param('id') id: string) {
-    return this.resumesService.findOne(+id);
+    return this.resumesService.findOne(id);
   }
 
+  @Post('by-user')
+  @ResponseMessage('Get Resumes by User')
+  getResumesByUser(@User() user: IUser) {
+    return this.resumesService.findByUsers(user);
+  }
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateResumeDto: UpdateResumeDto) {
-    return this.resumesService.update(+id, updateResumeDto);
+  @ResponseMessage('Update status resume')
+  update(
+    @Param('id') id: string,
+    @Body('status') status: string,
+    @User() user: IUser,
+  ) {
+    return this.resumesService.update(id, status, user);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.resumesService.remove(+id);
+  @ResponseMessage('Delete a resume by id')
+  remove(@Param('id') id: string, @User() user: IUser) {
+    return this.resumesService.remove(id, user);
   }
 }
